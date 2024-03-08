5771d995592368402d7d1cb4c1ea820a261d6c50
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-07 17:40:48 -0800

    Q 14 of Ts/js & node js of 45 assignment
    Q 14 of Ts/js & node js of 45 assignment

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.guest_list = void 0;
+var guest_list = ["Faria", "Sundas", "Nazish", "Bushra"];
+exports.guest_list = guest_list;
+for (var i = 0; i < guest_list.length; i++) {
+    console.log("Respeccted Sir/Madam" + guest_list[i] + guest_list, "\nWe invited you on dinner tomorrow.\n");
+}
---
+++ main.ts
@@ -0,0 +1,7 @@
+let guest_list : string [] = ["Faria","Sundas","Nazish","Bushra"];
+for(let i=0; i< guest_list.length; i++){
+    console.log(`Respeccted Sir/Madam` + guest_list[i] +guest_list,`\nWe invited you on dinner tomorrow.\n`);
+
+    }    export{guest_list};
+
+
